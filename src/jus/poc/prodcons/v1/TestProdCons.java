90a1ae113592741174a0ec20024097a54b0b5bae
Paul Labat <labat.paul@gmail.com>
2013-11-21 21:45:40 +0100

    Premier commmit, vérifie que tout fonctionne.

---
+++ src/jus/poc/prodcons/v1/TestProdCons.java
@@ -0,0 +1,24 @@
+package jus.poc.prodcons.v1;
+
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons.Simulateur;
+
+public class TestProdCons extends Simulateur {
+
+	public TestProdCons(Observateur observateur) {
+		super(observateur);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void run() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
