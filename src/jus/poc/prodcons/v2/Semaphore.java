5eab8569c56f2c54ff004099365c29068bb2240d
Paul Labat <labat.paul@gmail.com>
2013-11-25 20:28:26 +0100

    Maj de la class semaphore

--- src/jus/poc/prodcons/v2/Semaphore.java
+++ src/jus/poc/prodcons/v2/Semaphore.java
@@ -1,9 +1,27 @@
 package jus.poc.prodcons.v2;
 
+import java.util.ArrayList;
+
 public class Semaphore {
 
-	public Semaphore() {
-		// TODO Auto-generated constructor stub
+	private int cpt;
+	private ArrayList<Thread> liste;
+	
+	public Semaphore(int taille) {
+		cpt = taille;
+		liste = new ArrayList();
 	}
+	
+	public synchronized void p()
+	{
+		
+	}
+	
+	public synchronized void v()
+	{
+		
+	}
+	
+	
 
 }
