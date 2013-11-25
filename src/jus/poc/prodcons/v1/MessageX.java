cca513096160b9146ae34a685e6ecbf35683ae3b
Rodolphe <Rodolphe@192.168.1.14>
2013-11-25 21:18:20 +0100

    Correction d'un bug d'affichage

--- src/jus/poc/prodcons/v1/MessageX.java
+++ src/jus/poc/prodcons/v1/MessageX.java
@@ -15,7 +15,7 @@ public class MessageX implements Message {
 	
 	public String toString()
 	{
-		return "Le message "+numMsg+"est produit par "+idProd;
+		return "Le message "+numMsg+" est produit par "+idProd;
 		
 		
 	}
